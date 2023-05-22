e1531f08b748444dcd8076c3a67e5947b3959584
Rosenfeld99 <124917070+Rosenfeld99@users.noreply.github.com>
2023-05-22 13:59:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const fileUpload = require("express-fileupload");
+
+const { routesInit } = require("./routes/configRoutes");
+require("./db/mongoConnect");
+
+const app = express();
+
+app.use(
+  fileUpload({
+    limits: { fileSize: "5mb" },
+    useTempFiles: true,
+  })
+);
+// מאפשר לשלוח באדי דרך הצד לקוח
+app.use(express.json());
+
+// להגדיר תיקייה סטטית שתיהיה התיקייה בשם פאבליק
+app.use(express.static(path.join(__dirname, "public")));
+
+routesInit(app);
+
+const server = http.createServer(app);
+// בודק אם אנחנו על שרת אמיתי ואם כן דואג שנקבל את הפורט שהענן צריך
+// אם לא הברירת מחדל תיהיה 3003
+const port = process.env.PORT || 3003;
+server.listen(port);
---
+++ routes/uploads.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cloudinary = require("cloudinary").v2;
+const router = express.Router();
+const {config} = require("../config/secret")
+
+// Configuration
+cloudinary.config({
+  cloud_name: config.CLOUD_NAME,
+  api_key: config.CLOUD_KEY,
+  api_secret: config.TOKEN_SECRET,
+});
+
+router.get("/", async (req, res) => {
+  res.json({ msg: "uploads work" });
+});
+
+router.post("/cloud1", async(req,res) => {
+    try{
+      const myFile = req.files.myFile;
+      if(myFile){
+        const data = await cloudinary.uploader.upload(myFile.tempFilePath ,{ unique_filename:true})
+        // console.log(myFile);
+        res.json(data)
+  
+      }
+    }
+    catch(err){
+      console.log(err);
+      res.status(502).json({err})
+    }
+  })
+
+module.exports = router;
